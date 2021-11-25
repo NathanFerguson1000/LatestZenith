a25c1dfeb8b4b7d3d26bf8ac49bc8c0520f48f76
NathanFerguson1000 <72611364+NathanFerguson1000@users.noreply.github.com>
2021-11-25 13:21:29 +0200

    Update ALCandidatesCreateViewEditMarketingEventsCandidateEventPage.java

--- src/test/java/PageObjects/Sprint29/ALCandidatesCreateViewEditMarketingEventsCandidateEventPage.java
+++ src/test/java/PageObjects/Sprint29/ALCandidatesCreateViewEditMarketingEventsCandidateEventPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class >~]lALCandidatesCreateViewEditMarketingEventsCandidateEventPage {
+public class ALCandidatesCreateViewEditMarketingEventsCandidateEventPage {
     public WebDriver ldriver;
     public WebDriverWait wait;
 
